ca39be97cda62f6dc817ee3c3651179d01b4e7ce
Tim Gundmann <tim@gundmann.dk>
2017-07-23 07:53:55 +0200

    init of spring boot

---
+++ src/main/java/dk/gundmann/treatments/TreatmentsApplication.java
@@ -0,0 +1,13 @@
+package dk.gundmann.treatments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreatmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreatmentsApplication.class, args);
+	}
+
+}
